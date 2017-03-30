84cfa02969679edaed4cec274b2b86456ffa1040
226530 <226530@users.noreply.github.com>
2017-03-30 16:58:44 +0200

    Interfejsy
    Z zajęć

--- LAB4/ilist.hh
+++ LAB4/ilist.hh
@@ -6,9 +6,9 @@ class IList
 public: 
 	virtual void Add(unsigned int indeks,int wartosc) = 0;
 	virtual void Remove(unsigned int indeks) = 0;
-	virtual int Get(unsigned int indeks) = 0;
+	virtual int  Get(unsigned int indeks) = 0;
 	virtual unsigned int Size() = 0;
-
+	virtual void WykonajAlgorytm(unsigned int ile) = 0;
 };
 
 #endif
--- LAB4/iqueue.hh
+++ LAB4/iqueue.hh
@@ -5,7 +5,7 @@ class IQueue
 {
 public:
 	virtual void Enqueue(int wartosc) = 0;
-	virtual int Dequeue() = 0;
+	virtual int  Dequeue() = 0;
 	virtual unsigned int Size() = 0;
 	virtual void WykonajAlgorytm(unsigned int ile ) = 0;
 
